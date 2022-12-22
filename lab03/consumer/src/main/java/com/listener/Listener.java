5dd716cde8821a7f44efee6f3a6cbdb91d32fbf2
IgorSytnik <kintys.sytnik@gmail.com>
2022-12-22 17:38:02 +0200

    minor changes -removed unneeded parts

--- lab03/consumer/src/main/java/com/listener/Listener.java
+++ lab03/consumer/src/main/java/com/listener/Listener.java
@@ -5,7 +5,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.repository.Repository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
